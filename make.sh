a0e4dbe1425d9a0480639e8e5da64f284e2799a3
Diman <phelonc33m@gmail.com>
2023-09-09 18:08:31 +0300

    Commit.

--- make.sh
+++ make.sh
@@ -2,7 +2,23 @@ GNULatestToolchain="https://developer.arm.com/-/media/Files/downloads/gnu/12.3.r
 GNUToolchainFolder="./toolchain"
 echo Checking to GNU Toolchain...
 
+if [ -d $GNUToolchainFolder ]
+then 
+    echo OK
+else
+    echo Not found
+    echo Downloading ARM Toolchain
+    curl -o toolchain.zip -L $GNULatestToolchain
+    mkdir $GNUToolchainFolder
+    unzip ./toolchain.zip "arm-gnu-toolchain-*/*" -d "$GNUToolchainFolder"
+    rm -rf toolchain.zip
+    echo Restart script please.
+    exit 1
+fi
 
 echo Building..
 
 cd boot && chmod +x make.sh && ./make.sh && cd ..
+cd krnl && chmod +x make.sh && ./make.sh && cd ..
+cd sm && chmod +x make.sh && ./make.sh && cd ..
+cd trusted_krnl && chmod +x make.sh && ./make.sh && cd ..
